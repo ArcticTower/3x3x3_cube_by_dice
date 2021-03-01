2c84a74d5ef9c47daa0f4dcce6f77d5ed202738d
ArcticTower <perf@live.ru>
2021-03-01 17:05:04 +0300

    new dice shuffle algorithm

--- src/Dice.java
+++ src/Dice.java
@@ -65,31 +65,88 @@ public class Dice {
         back = buf;
 
     }
+    
+    private void rotateUp(){
+
+        int buf = up;
+        up = front;
+        front = down;
+        down = back;
+        back = buf;
+
+    }
+
+    private void rotateLeft(){
+
+        int buf = left;
+        left = front;
+        front = right;
+        right = back;
+        back = buf;
+
+    }
+
+    private void rotateRed(){
+
+        int buf = right;
+        right = up;
+        up = left;
+        left = down;
+        down = buf;
+
+    }
 
     public void shuffle(){
 
         Random random = new Random();
-        int r = random.nextInt(4);
-        int d = random.nextInt(4);
-        int r2 = random.nextInt(4);
-
-        for(int i =0;i<r;i++){
-
-            rotateRight();
-
+        int front = random.nextInt(6)+1;
+        int red = random.nextInt(4);
+        switch (front){
+            case 1:
+                break;
+            case 2:
+                rotateUp();
+                break;
+            case 3:
+                rotateLeft();
+                break;
+            case 4:
+                rotateRight();
+                break;
+            case 5:
+                rotateDown();
+            case 6:
+                rotateDown();
+                rotateDown();
+                break;
+                
         }
 
-        for(int i =0;i<d;i++){
-
-            rotateDown();
-
+        for (int i =0;i<red;i++){
+            rotateRed();
         }
 
-        for(int i =0;i<r2;i++){
-
-            rotateRight();
-
-        }
+//        int r = random.nextInt(4);
+//        int d = random.nextInt(4);
+//        int r2 = random.nextInt(4);
+//
+//        for(int i =0;i<r;i++){
+//
+//            rotateRight();
+//
+//        }
+//
+//        for(int i =0;i<d;i++){
+//
+//            rotateDown();
+//
+//        }
+//
+//        for(int i =0;i<r2;i++){
+//
+//            rotateRight();
+//
+//        }
 
     }
 
